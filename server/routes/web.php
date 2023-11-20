a84e02f4c75dc952b94a8b17ec31e031d273c7f3
LGBK23 <0306211114@caothang.edu.vn>
2023-11-20 13:04:31 +0700

    DOAN

---
+++ server/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('ADMIN/trang-chu');
+});
+Route::get('/danh-sach-san-pham', function () {
+    return view('SANPHAM/danh-sach');
+});
+Route::get('/danh-sach-loai', function () {
+    return view('LOAI/danh-sach');
+});
+Route::get('/danh-sach-mau', function () {
+    return view('MAU/danh-sach');
+});
+Route::get('/danh-sach-size', function () {
+    return view('SIZE/danh-sach');
+});
